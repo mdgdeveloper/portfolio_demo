7da07f97dd2674dec47e039bf23bd12d4f5ed7ec
Sergio Madrigal <se.rgiomadrigal@gmail.com>
2021-10-18 13:04:49 +0200

    Cookies defined and added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import { useContext } from "react"
-import About from "./components/about/About"
-import Contact from "./components/contact/Contact"
-import Intro from "./components/intro/Intro"
-import ProductList from "./components/productList/ProductList"
-import Toggle from "./components/toggle/Toggle"
-import { ThemeContext } from "./context"
-
-
+import { useContext } from "react";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Cookies from "./components/cookies/Cookies";
+import Intro from "./components/intro/Intro";
+import ProductList from "./components/productList/ProductList";
+import Toggle from "./components/toggle/Toggle";
+import { ThemeContext } from "./context";
 
 const App = () => {
-
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
 
   return (
-    <div style={{ backgroundColor: darkMode ? "#222" : "white", color: darkMode ? "white" : "black"}}>
+    <div
+      style={{
+        backgroundColor: darkMode ? "#222" : "white",
+        color: darkMode ? "white" : "black",
+      }}
+    >
       <Toggle />
       <Intro />
       <About />
       <ProductList />
       <Contact />
+      <Cookies />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
